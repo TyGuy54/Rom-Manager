542e17db3c7a27931b2b22ddd6c4df844b5ad21e
Tyler James Ross <tyguy@Tylers-MBP.lan>
2023-12-26 19:33:14 -0800

    updared ui

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::fs;
+use std::env;
 use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
 use std::process::Command;
@@ -14,7 +15,6 @@ use serde::{Deserialize, Serialize};
 enum Value {
     Path(PathBuf),
     String(String),
-    OsStr(String),
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -36,7 +36,7 @@ impl serde::Serialize for Error {
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn load_rom(rom_name: &str) {
-    let output = Command::new("xdg-open")
+    let output = Command::new("open") // this is macs version of xdg-open for linux
     .arg(rom_name)
     .output()
     .expect("failed to execute process");
@@ -45,10 +45,30 @@ fn load_rom(rom_name: &str) {
 }
 
 #[tauri::command]
+fn get_imgs() -> Result<Vec<HashMap<&'static str, Value>>, Error> {
+    let mut cool_vec = Vec::new();
+    let paths = fs::read_dir("../ROM-imgs/GBA").unwrap();
+
+    for imgs in paths {
+        let mut card_data_payload = HashMap::new();
+
+        let img_location = imgs.unwrap();
+        let img_path = img_location.path();
+        let img_name = img_path.file_name().unwrap().to_owned();
+
+        card_data_payload.insert("img_path", Value::Path(img_path));
+        card_data_payload.insert("img_name", Value::String(img_name.into_string().unwrap()));
+        cool_vec.push(card_data_payload);
+    }
+
+    return Ok(cool_vec);
+}
+
+
+#[tauri::command]
 fn card_data() -> Result<Vec<HashMap<&'static str, Value>>, Error>{
     let mut cool_vec = Vec::new();
-
-    let paths = fs::read_dir("").unwrap(); // nned to set path to ROMS/GBA
+    let paths = fs::read_dir("../ROMS/GBA").unwrap();
 
     for roms in paths {
         let mut card_data_payload = HashMap::new();
@@ -64,7 +84,7 @@ fn card_data() -> Result<Vec<HashMap<&'static str, Value>>, Error>{
         match rom_file_extension.unwrap() {
             "gba" => { 
                 card_data_payload.insert("rom_path", Value::Path(rom_path.clone()));
-                card_data_payload.insert("rom_name", Value::OsStr(rom_name.into_string().unwrap()));
+                card_data_payload.insert("rom_name", Value::String(rom_name.into_string().unwrap()));
 
                 cool_vec.push(card_data_payload)
             },
@@ -80,7 +100,7 @@ fn card_data() -> Result<Vec<HashMap<&'static str, Value>>, Error>{
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![load_rom, card_data])
+        .invoke_handler(tauri::generate_handler![load_rom, card_data, get_imgs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,113 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import "./card.css";
 
 function App() {
   const [cardDatas, setCardData] = useState([]);
+  const [romImgs, setRomImgs] = useState([]);
 
   useEffect(() => {
     async function cardData() {
       // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-      setCardData(await invoke("card_data", {}))
+      setCardData(await invoke("card_data", {}));
     }
 
-    cardData().catch(console.error)
-  }, [])
+    async function getImgs() {
+      setRomImgs(await invoke("get_imgs", {}));
+    }
+
+    cardData().catch(console.error);
+    getImgs().catch(console.error)
+  }, []);
 
   async function loadRom(romName) {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    await invoke("load_rom", { romName }).then((data) => {
-      console.log(`This is from loadRom ${data}`)
-    }
-    ) .catch((error) => {
+    await invoke("load_rom", { romName })
+      .then((data) => {
+        console.log(`This is from loadRom ${data}`);
+      })
+      .catch((error) => {
         console.log(error);
-      }
-    );
+      });
+  }
+
+  const setImg = (romName) => {
+    return romImgs.map((obj, i) => {
+      let img_path = obj.img_path;
+      let img_name = obj.img_name;
+      return Object.keys(img_name).map((img, i) => {
+        return Object.keys(img_path).map((pth, i) => {
+          if (img_name[img].replace(".jpg", "") == romName.replace(".gba", "")) {
+            return (
+              <img
+                src={img_path[pth]}
+                class="card__image"
+                alt="rom img"
+              />
+            );
+          } 
+        });
+      });
+    });
   }
 
   const makeCards = () => {
     return cardDatas.map((data, i) => {
-      let path = data.rom_path
-      let name = data.rom_name
+      let path = data.rom_path;
+      let name = data.rom_name;
       return Object.keys(path).map((obj, i) => {
-          return(
-            <div className="card" key={i} onClick={(e) => {
-              e.preventDefault();
-              loadRom(path[obj]);
-            }}>
-              <div className="container">
-                  {Object.keys(name).map((obj, i) => {
-                    return <h4>{name[obj]}</h4>
-                  })}
+        return (
+          <>
+            <li>
+              <div
+                className="card"
+                key={i}
+                onClick={(e) => {
+                  e.preventDefault();
+                  loadRom(path[obj]);
+                }}
+              >
+                {Object.keys(name).map((obj, i) => {
+                  return (
+                    setImg(name[obj])
+                  );
+                })}
+                <div className="card__overlay">
+                  <div className="card__header">
+                    <div className="card__header-text">
+                      <h3 className="card__title">Game Boy Advanced</h3>
+                      {/* <span className="card__status">1 hour ago</span> */}
+                    </div>
+                  </div>
+                  <div className="container">
+                    {Object.keys(name).map((obj, i) => {
+                      return <h4>{name[obj]}</h4>;
+                    })}
+                  </div>
+                </div>
               </div>
-          </div> 
-          );
-        })
-    })
-  }
+            </li>
+          </>
+        );
+      });
+    });
+  };
 
   return (
     <div className="info-grid-container">
       <h2 className="header-pos">Rom Manager</h2>
 
-      <div className="wrap">
-      <div className="search">
+      {/* <div className="wrap">
+        <div className="search">
           <input type="text" className="searchTerm" placeholder="What are you looking for?"/>
-          <button type="submit" className="searchButton">s</button>
-      </div>
-    </div>
+          <button type="submit" className="searchButton">search</button>
+        </div>
+      </div> */}
 
       <div className="content-pos">
-          {makeCards()}
+        <ul className="cards">{makeCards()}</ul>
+
       </div>
     </div>
   );
